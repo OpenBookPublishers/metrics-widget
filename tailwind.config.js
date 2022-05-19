bd438ba292fe30fb91d26ec9d1da24c85c56c04d
Javier Arias <javier@jarias.org>
2022-05-19 13:31:51 +0100

    Upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: ['./lib/**/*.{js,jsx,ts,tsx}']
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ['./lib/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
